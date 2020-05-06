f630f7cf97cb332bf8e4cf5044767aae394d5f9d
Adham Naeim <dodo.yang2@gmail.com>
2020-05-06 22:17:50 +0200

    1 removed scripts routed to src and rerouted them to dist, 2- add babel and its presets, 3-added imports/exports to our client js code, 4- install babel-loader rules.

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,3 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
 alert("hello. Im under the code, please help me. OOOOoooOOoOOOoo")
--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,13 @@ const webpack = require("webpack")
 
 module.exports ={
     entry: "./src/client/index.js", //changed the default entry point from ./src/index.js
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude:/node_modules/,    //babel loader rules
+                loader: "babel-loader"
+            }
+        ]
+    }
 }
