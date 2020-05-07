00e057c5b58c92e491118d97c9ac092460cf9f85
Adham Naeim <dodo.yang2@gmail.com>
2020-05-07 23:39:28 +0200

    installed aylien api, installed and integrated (css-minification,terser and optimize-css) plugins to minify the main.css file! (skipping service workers)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,26 @@
 const path = require("path")
 const webpack = require("webpack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports ={
     mode: "production",
     entry: "./src/client/index.js", //changed the default entry point from ./src/index.js
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude:/node_modules/,    //babel loader rules
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ],
             },
         ]
     },
@@ -20,5 +30,6 @@ module.exports ={
           template: './src/client/views/index.html',
           filename: './index.html'
         }),
+        new MiniCssExtractPlugin({filename: '[name].css'})
       ],
 }
