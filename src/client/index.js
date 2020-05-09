834103af9b197240b109265388963b4679321669
Adham Naeim <dodo.yang2@gmail.com>
2020-05-10 01:34:51 +0200

    removed silly alert for testing

--- src/client/index.js
+++ src/client/index.js
@@ -5,7 +5,6 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
 import './styles/form.scss'
-alert("hello. Im under the code, please help me. OOOOoooOOoOOOoo")
 
 export{
     checkForUrl,
